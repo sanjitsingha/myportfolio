b266ebf6e748cc2a70f10be71f445044a149e76f
sanjitsingha <111037110+sanjitsingha@users.noreply.github.com>
2023-06-20 13:43:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+menu = document.querySelector("#menu_btn");
+nav = document.querySelector("#nav-m");
+body  = document.querySelector("#body");
+menu.onclick = function (){
+    
+    nav.classList.toggle("active");
+};
+
