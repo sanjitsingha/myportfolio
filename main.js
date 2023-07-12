14894da2e11afd589715f810622afd97ebfa1863
sanjitsingha <111037110+sanjitsingha@users.noreply.github.com>
2023-07-12 10:44:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ menu.onclick = function (){
     nav.classList.toggle("active");
 };
 
+let crsr = document.querySelector(".cursor");
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x +  "px"
+    crsr.style.top = dets.y + "px"
+
+})
